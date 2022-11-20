d4e42799b00674a26bd3705493872953b7b0ebc9
Lauriacunia <lauriacunia@gmail.com>
2022-11-19 21:53:43 -0300

    agrego ejemplo de admin

--- src/routes/carritoRoutes.js
+++ src/routes/carritoRoutes.js
@@ -2,13 +2,25 @@ const { Router } = require("express");
 const router = Router();
 const file = "src/carrito.txt";
 const Container = require("../persistence/container.js");
-const containerProducts = new Container();
+const container = new Container();
 const myScript = 'public/main.js';
 
+const admin = false;
+
 router.get("/", (req, res) => {
-   const carrito = null;
-   content = "carrito";
-   res.render('index.ejs', { carrito, myScript, content })
+   if(admin){
+      const carrito = null;
+      const mensaje = null;
+      content = "carrito";
+      res.render("index.ejs", { carrito, myScript, content, mensaje });
+   }else{
+      //res.redirect("/login");
+      const carrito = null;
+      content = "carrito";
+      const mensaje = "No tiene permisos para acceder a esta sección";
+      res.render("index.ejs", { carrito, myScript, content, mensaje });
+   }
+  
 });
 
 
